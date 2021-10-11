3dca36ba5ddc2a97a0edf0f14e127456a3e0895b
david7838 <forbid7838@gmail.com>
2021-10-11 22:28:14 +0800

    demo05: container - margin & padding

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.indigo[300],
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey[200],
-            title: const Text('My App'),
+        backgroundColor: Colors.indigo[300],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[200],
+          title: const Text('My App'),
+        ),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            // margin: const EdgeInsets.all(20.0),
+            // margin: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
+            // margin: const EdgeInsets.fromLTRB(100.0, 40.0, 50.0, 200.0),
+            margin: const EdgeInsets.only(left: 200.0),
+            padding: const EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: const Text('Hello'),
           ),
-          body: Container()),
+        ),
+      ),
     );
   }
 }
