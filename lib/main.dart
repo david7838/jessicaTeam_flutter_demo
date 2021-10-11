dca6e2ab3ac57596b1c3c4303e1705e5927d18cf
david7838 <forbid7838@gmail.com>
2021-10-11 21:25:02 +0800

    demo04: hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.indigo[100],
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          title: const Text('My App'),
-        ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/cat_demo.jpeg'),
+          backgroundColor: Colors.indigo[300],
+          appBar: AppBar(
+            backgroundColor: Colors.blueGrey[200],
+            title: const Text('My App'),
           ),
-        ),
-      ),
-    ),
-  );
+          body: Container()),
+    );
+  }
 }
