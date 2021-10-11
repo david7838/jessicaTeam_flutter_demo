81bfdf37ab716f08c907f24bc29152e4567bafb5
david7838 <forbid7838@gmail.com>
2021-10-11 21:02:22 +0800

    demo02: scaffold & image

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text('Hello World'),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: const Text('My App'),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/1/17/Google-flutter-logo.png'),
+          ),
+        ),
+      ),
     ),
-  ));
+  );
 }
