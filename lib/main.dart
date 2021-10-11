69513460b81fee708691e1c056da6c89dc8df793
david7838 <forbid7838@gmail.com>
2021-10-11 22:59:38 +0800

    demo06: column & row

--- lib/main.dart
+++ lib/main.dart
@@ -17,16 +17,41 @@ class MyApp extends StatelessWidget {
           title: const Text('My App'),
         ),
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            // margin: const EdgeInsets.all(20.0),
-            // margin: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
-            // margin: const EdgeInsets.fromLTRB(100.0, 40.0, 50.0, 200.0),
-            margin: const EdgeInsets.only(left: 200.0),
-            padding: const EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: const Text('Hello'),
+          child: Column(
+            //mainAxisSize: MainAxisSize.min,           //確認layout
+            // verticalDirection: VerticalDirection.up, //從下往上開始
+            // mainAxisAlignment: MainAxisAlignment.end, //最下方開始對齊
+            // mainAxisAlignment: MainAxisAlignment.center, //中間開始對齊
+            //mainAxisAlignment: MainAxisAlignment.spaceBetween, //平均分割
+            crossAxisAlignment: CrossAxisAlignment
+                .end, //新增一個container with inifinty width,強制讓所有container靠右對齊
+            children: [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: const Text('Container 1 '),
+              ),
+              const SizedBox(
+                height: 50.0,
+              ), //增加空間
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blueAccent,
+                child: const Text('Container 2 '),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+                child: const Text('Container 3 '),
+              ),
+              //Container(
+              //  width: double.infinity,
+              //  height: 10.0,
+              //)
+            ],
           ),
         ),
       ),
