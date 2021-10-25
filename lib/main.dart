2400a41d770a525a8664b0d7cceb50184b15e9cd
david7838 <forbid7838@gmail.com>
2021-10-25 21:22:34 +0800

    fonts

--- lib/main.dart
+++ lib/main.dart
@@ -13,34 +13,65 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 100.0,
-                color: Colors.red,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/cat_demo.jpeg'),
+            ),
+            Text(
+              'Catty',
+              style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.teal.shade100,
+                  fontFamily: 'Pacifico'),
+            ),
+            const Text(
+              'Software Developer',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Source Sans Pro',
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              Container(
-                width: 100.0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.yellow,
-                    ),
-                    Container(width: 100.0, height: 100.0, color: Colors.green)
-                  ],
+            ),
+            SizedBox(
+              height: 30.0,
+              width: 200.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+                thickness: 3.0,
+              ),
+            ),
+            const Card(
+              color: Colors.white,
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'xxx@gmail.com',
+                  style: TextStyle(fontFamily: 'Source Sans Pro'),
                 ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
-              )
-            ],
-          ),
-        ),
+            ),
+            const Card(
+              color: Colors.white,
+              margin: EdgeInsets.all(10.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text('0912-345-678'),
+              ),
+            )
+          ],
+        )),
       ),
     );
   }
